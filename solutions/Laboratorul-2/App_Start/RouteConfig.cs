fd8784f4b51e18b9180b44357f04b78ad27729a7
Stefan <iordachestefan1@gmail.com>
2020-11-01 18:43:44 +0200

    NEW - Laborator 2 - rezolvare

---
+++ solutions/Laboratorul-2/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Laboratorul_2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Constrangerea de la exercitiul 3 poate fi aplicata in acest configurator
+            // ATENTIE LA PLASARE! Constrangerile se executa in mod cascada, de sus in jos
+            routes.MapRoute(
+                name: "exercitiul3-regex",
+                url: "laborator2/exercitiul3/{numar}",
+                defaults: new { controller = "Laborator2", action = "Exercitiul3", numar = UrlParameter.Optional},
+                /*
+                 * Constrangerea functioneaza astfel:
+                 * ^ - marcheaza inceputul cuvantului
+                 * [1-9] - specificam conditia de inceput pentru orice numar: un caracter cuprins intre 1 i 9 (cifra diferita de 0)
+                 * \d - orice digit (sintaxa echivalenta cu [0-9])
+                 * {1,5} - specificam numarul de caractere de tipului ultimului token scris (in cazul nostru \d)
+                 * [02468] - conditia de terminare a unui numar par, specificata printr-o lista de caractere
+                 * $ - final de sir/string/linie
+                 */
+                constraints: new { numar = @"^[1-9]\d{1,5}[02468]$"}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ solutions/Laboratorul-2/Controllers/Laborator2Controller.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorul_2.Controllers
+{
+    public class Laborator2Controller : Controller
+    {
+
+        // GET: /laborator2/exercitiul1/?word1=ceva&word2=altceva
+        public ActionResult Exercitiul1(string word1, string word2)
+        {
+            // Tratam orice exceptie prin utilizarea blocului try-catch
+            try
+            {
+                word1 = word1.ToLower();
+                word2 = word2.ToLower();
+
+                // Plasam in ViewData (sau ViewBag) mesajul pentru utilizator
+                ViewData["message"] = word1.Contains(word2) || word2.Contains(word1);
+            }
+            // Exceptia aceasta este tratata in cel mai general mod posibil
+            catch (Exception e)
+            {
+                ViewData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+
+        // GET: /laborator2/exercitiul2/?word1=ceva&word2=altceva
+        // Optionalitatea celui de al doilea parametru este data in mod implicit de tipul de date string
+        // Pentru variabile de tip int, float, DateTime, etc. se poate utiliza sintaxa "int? numar"
+        public ActionResult Exercitiul2(string word1, string word2)
+        {
+            // Returnam un mesaj utilizatorului, in acest caz folosind o eroare predefinita: 404
+            if (word2 == null)
+            {
+                return HttpNotFound("Al doilea parametru lipseste");
+            }
+
+            try
+            {
+                word1 = word1.ToLower();
+                word2 = word2.ToLower();
+
+                ViewData["message"] = word1.Contains(word2) || word2.Contains(word1);
+            }
+            catch (Exception e)
+            {
+                ViewData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+
+        // GET: /laborator2/exercitiul3/100
+        public ActionResult Exercitiul3(int numar)
+        {
+            return View();
+        }
+
+
+        // GET: /laborator2/exercitiul4farapartialview/?numere=7&numere=3&numere=14
+        public ActionResult Exercitiul4FaraPartialView(List<int> numere)
+        {
+            try
+            {
+                ViewData["numere"] = numere;
+            }
+            catch (Exception e)
+            {
+                // Putem returna direct un cod HTTP
+                return new HttpStatusCodeResult(500);
+            }
+
+            return View();
+        }
+
+        // GET: /laborator2/exercitiul4/?numere=7&numere=3&numere=14
+        public ActionResult Exercitiul4(List<int> numere)
+        {
+            try
+            {
+                ViewData["numere"] = numere;
+            }
+            catch (Exception e)
+            {
+                // Putem returna direct un cod HTTP
+                return new HttpStatusCodeResult(500);
+            }
+
+            return View();
+        }
+
+    }
+}
